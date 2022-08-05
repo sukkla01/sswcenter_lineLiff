c5f6307dfdbbd895f429d7ef5a8dfb0b8fcd3b30
sukkla01 <sukkla01@gmail.com>
2022-08-05 11:30:54 +0700

    ป

---
+++ pages/qa.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import NavHeader from '../component/NavHeader'
+
+const Qa = () => {
+    return (
+        <div style={{ textAlign: "left" }}>
+            <NavHeader title='ถาม-ตอบ' />
+            <div style={{ paddingTop: '18%' }}>
+                <div className='main-wrapper' style={{ backgroundColor: 'white', marginLeft: 10, marginRight: 10, borderRadius: 15 }}>
+                    <div className='mt-10'>
+                        <p >รออัพเดทข้อมูล</p>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Qa
