d673fc05a0ee46caca4b3cd5c40eec11ef971861
sukkla01 <sukkla01@gmail.com>
2022-08-05 10:13:39 +0700

    x

--- pages/complain.js
+++ pages/complain.js
@@ -103,8 +103,8 @@ function Complain() {
                         console.log(isOK)
                         const liff = (await import('@line/liff')).default
                         await liff.ready
-                        await axios.get(`https://sw-center-line.diligentsoftinter.com/rely_m/${formData.user_id}`)
                         liff.closeWindow()
+                        await axios.get(`https://sw-center-line.diligentsoftinter.com/rely_m/${formData.user_id}`)
                     }
 
                 } catch (error) {
