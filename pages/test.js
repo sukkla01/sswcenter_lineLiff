28e5a5232c19f98f5fec42d544d717ef463f7c14
sukkla01 <sukkla01@gmail.com>
2022-08-22 10:32:42 +0700

    chat

--- pages/test.js
+++ pages/test.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import io from 'socket.io-client';
+import NavHeader from '../component/NavHeader';
+import config from '../config'
 
+const BASE_URL = config.BASE_URL
 
-const socket = io(`https://sw-center-api.diligentsoftinter.com`);
+const socket = io(BASE_URL);
 const i = 1
 const Test = () => {
     const [isConnected, setIsConnected] = useState(socket.connected);
@@ -37,7 +40,7 @@ const Test = () => {
 
     return (
         <div>
-            <p>Connected: {'' + isConnected}</p>
+            {/* <p>Connected: {'' + isConnected}</p>
             <input onChange={e => {
                 // setIsCode(false)
                 setText(e.target.value)
@@ -45,7 +48,94 @@ const Test = () => {
             }}
             />
             <p>Last pong: {text}</p>
-            <button onClick={sendPing}>Send ping</button>
+            <button onClick={sendPing}>Send ping</button> */}
+            <NavHeader />
+            <div className="page-content page-container" id="page-content" >
+                    <div className="row container d-flex justify-content-center" style={{ paddingTop: 80 }}>
+                        <div className="col-md-6">
+                            <div className="card card-bordered">
+                                <div className="card-header">
+                                    <h4 className="card-title"><strong>Chat</strong></h4>
+                                    {/* <a className="btn btn-xs btn-secondary" href="#" data-abc="true">Let's Chat App</a> */}
+                                </div>
+                                <div className="ps-container ps-theme-default ps-active-y" id="chat-content" style={{ overflowY: 'scroll !important', height: '400px !important' }}>
+                                    <div className="media media-chat">
+                                        <img className="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
+                                        <div className="media-body">
+                                            <p>Hi</p>
+                                            <p>How are you ...???</p>
+                                            <p>What are you doing tomorrow?<br /> Can we come up a bar?</p>
+                                            <p className="meta"><time dateTime={2018}>23:58</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="media media-meta-day">Today</div>
+                                    <div className="media media-chat media-chat-reverse">
+                                        <div className="media-body">
+                                            <p>Hiii, I'm good.</p>
+                                            <p>How are you doing?</p>
+                                            <p>Long time no see! Tomorrow office. will be free on sunday.</p>
+                                            <p className="meta"><time dateTime={2018}>00:06</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="media media-chat">
+                                        <img className="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
+                                        <div className="media-body">
+                                            <p>Okay</p>
+                                            <p>We will go on sunday? </p>
+                                            <p className="meta"><time dateTime={2018}>00:07</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="media media-chat media-chat-reverse">
+                                        <div className="media-body">
+                                            <p>That's awesome!</p>
+                                            <p>I will meet you Sandon Square sharp at 10 AM</p>
+                                            <p>Is that okay?</p>
+                                            <p className="meta"><time dateTime={2018}>00:09</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="media media-chat">
+                                        <img className="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
+                                        <div className="media-body">
+                                            <p>Okay i will meet you on Sandon Square </p>
+                                            <p className="meta"><time dateTime={2018}>00:10</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="media media-chat media-chat-reverse">
+                                        <div className="media-body">
+                                            <p>Do you have pictures of Matley Marriage?</p>
+                                            <p className="meta"><time dateTime={2018}>00:10</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="media media-chat">
+                                        <img className="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
+                                        <div className="media-body">
+                                            <p>Sorry I don't have. i changed my phone.</p>
+                                            <p className="meta"><time dateTime={2018}>00:12</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="media media-chat media-chat-reverse">
+                                        <div className="media-body">
+                                            <p>Okay then see you on sunday!!</p>
+                                            <p className="meta"><time dateTime={2018}>00:12</time></p>
+                                        </div>
+                                    </div>
+                                    <div className="ps-scrollbar-x-rail" style={{ left: 0, bottom: 0 }}><div className="ps-scrollbar-x" tabIndex={0} style={{ left: 0, width: 0 }} /></div><div className="ps-scrollbar-y-rail" style={{ top: 0, height: 0, right: 2 }}><div className="ps-scrollbar-y" tabIndex={0} style={{ top: 0, height: 2 }} /></div></div>
+                                <div className="publisher bt-1 border-light">
+                                    <img className="avatar avatar-xs" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
+                                    <input className="publisher-input" type="text" placeholder="Write something" />
+                                    <span className="publisher-btn file-group">
+                                        <i className="fa fa-paperclip file-browser" />
+                                        <input type="file" />
+                                    </span>
+                                    <a className="publisher-btn" href="#" data-abc="true"><i className="fa fa-smile" /></a>
+                                    <a className="publisher-btn text-info" href="#" data-abc="true"><i className="fa fa-paper-plane" /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+
+
         </div>
     )
 }
