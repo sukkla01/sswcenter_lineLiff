4c0824fafbdc18e776b2243ebf37c89b4f5be5fb
sukkla01 <sukkla01@gmail.com>
2022-08-22 10:51:47 +0700

    x

--- pages/test.js
+++ pages/test.js
@@ -58,7 +58,7 @@ const Test = () => {
                                 <h4 className="card-title"><strong>Chat</strong></h4>
                                 {/* <a className="btn btn-xs btn-secondary" href="#" data-abc="true">Let's Chat App</a> */}
                             </div>
-                            <div className="ps-container ps-theme-default ps-active-y" id="chat-content" style={{ overflowY: 'scroll !important', height: '400px !important' }}>
+                            <div className="ps-container ps-theme-default ps-active-y" id="chat-content" style={{ overflowY: 'scroll !important', height: '100vh !important' }}>
                                 <div className="media media-chat">
                                     <img className="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
                                     <div className="media-body">
@@ -67,19 +67,41 @@ const Test = () => {
                                     </div>
                                 </div>
                                 {/* <div className="media media-meta-day">Today</div> */}
+
+
+
                                 <div className="media media-chat media-chat-reverse">
                                     <div className="media-body">
-
-                                        <p>Long time no see! Tomorrow office. will be free on sunday.</p>
-                                        <p className="meta"><time dateTime={2018}>00:06</time></p>
+                                        <p>Do you have pictures of Matley Marriage?</p>
+                                        <p className="meta"><time dateTime={2018}>00:10</time></p>
                                     </div>
                                 </div>
                                 <div className="media media-chat">
                                     <img className="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
                                     <div className="media-body">
-                                        <p>Okay</p>
+                                        <p>Hi</p>
+
                                     </div>
                                 </div>
+                                <div className="media media-chat media-chat-reverse">
+                                    <div className="media-body">
+                                        <p>Do you have pictures of Matley Marriage?</p>
+                                        <p className="meta"><time dateTime={2018}>00:10</time></p>
+                                    </div>
+                                </div>
+
+                                <div className="ps-scrollbar-x-rail" style={{ left: 0, bottom: 0 }}><div className="ps-scrollbar-x" tabIndex={0} style={{ left: 0, width: 0 }} /></div><div className="ps-scrollbar-y-rail" style={{ top: 0, height: 0, right: 2 }}><div className="ps-scrollbar-y" tabIndex={0} style={{ top: 0, height: 2 }} />
+                                </div>
+                            </div>
+                            <div className="publisher bt-1 border-light">
+                                <img className="avatar avatar-xs" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..." />
+                                <input className="publisher-input" type="text" placeholder="Write something" />
+                                <span className="publisher-btn file-group">
+                                    <i className="fa fa-paperclip file-browser" />
+                                    <input type="file" />
+                                </span>
+                                <a className="publisher-btn" href="#" data-abc="true"><i className="fa fa-smile" /></a>
+                                <a className="publisher-btn text-info" href="#" data-abc="true"><i className="fa fa-paper-plane" /></a>
                             </div>
                         </div>
                     </div>
